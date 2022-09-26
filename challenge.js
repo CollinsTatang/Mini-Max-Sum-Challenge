e29dbf25ff2d894a311674844fb2608d8c332ad0
CollinsTatang <makungongcollins@gmail.com>
2022-09-26 19:55:53 +0100

    Challege solve

---
+++ challenge.js
@@ -0,0 +1,20 @@
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+
+    let totals = [];
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+    for (let i = 0; i < arr.length; i++) {
+        let filtered = arr.filter(function(value, index, arr) {
+            return i !== index;
+        });
+        totals.push(filtered.reduce(reducer));
+    }
+    
+    console.log(Math.min(...totals) + " " + Math.max(...totals));
+}
